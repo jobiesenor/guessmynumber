7aeb931592ecaec57c33953f3ab089468221717f
Josiah Lawhorn <jobiesenor@gmail.com>
2021-07-15 17:55:43 -0700

    Guessing game complete Not Refactored

--- starter/script.js
+++ starter/script.js
@@ -21,7 +21,11 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20; // known as a state variable. Updates with the state. 
+let highScore = 0; 
 
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}  // refactoring strategy. see old versions for reference. 
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
@@ -29,40 +33,54 @@ document.querySelector('.check').addEventListener('click', function(){
 
     //when there is no input
     if(!guess) {
-        document.querySelector('.message').textContent = '⛔️ No Number'; 
+        displayMessage('⛔️ No Number'); 
 
     // when the player wins   
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number🎉';
         document.querySelector('.number').textContent = secretNumber;
 
-        
         document.querySelector('body').style.backgroundColor = '#60b347';
-
         document.querySelector('.number').style.width = '30rem';
-    // when the guess is too high
-    } else if (guess > secretNumber){
 
-        if(score > 0) {
-        document.querySelector('.message').textContent = 'Too high 📈'; 
-        score--;
-        document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = '😔 You lost the game!';
-            document.querySelector('.score').textContent = 0;
+        if( score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
         }
+
+    // when guess is wrong
+    } else if(guess !== secretNumber) {
+        if(score > 1) {
+            document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too high!' : 'Too low 📉!'; //terenary operator 
+            score--;
+            document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = '😔 You lost the game!';
+                document.querySelector('.score').textContent = 0;
+            }
+    // when the guess is too high      
+    // } else if (guess > secretNumber){
+
+    //     if(score > 1) {
+    //     document.querySelector('.message').textContent = '📈 Too high!'; 
+    //     score--;
+    //     document.querySelector('.score').textContent = score;
+    //     } else {
+    //         document.querySelector('.message').textContent = '😔 You lost the game!';
+    //         document.querySelector('.score').textContent = 0;
+    //     }
       
-     //when guess is too low   
-    } else if (guess < secretNumber){
+    //  //when guess is too low   
+    // } else if (guess < secretNumber){
 
-        if(score > 0 ){
-        document.querySelector('.message').textContent = 'Too low 📉'; 
-        score--;
-        document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = '😔 You lost the game!';
-            document.querySelector('.score').textContent = 0;
-        }
+    //     if(score > 1 ){
+    //     document.querySelector('.message').textContent = 'Too low 📉!'; 
+    //     score--;
+    //     document.querySelector('.score').textContent = score;
+    //     } else {
+    //         document.querySelector('.message').textContent = '😔 You lost the game!';
+    //         document.querySelector('.score').textContent = 0;
+    //     }
     }
 });
 
@@ -85,6 +103,7 @@ GOOD LUCK 😀
 */
 
 document.querySelector('.again').addEventListener('click', function() {
+    
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
 
@@ -95,5 +114,10 @@ document.querySelector('.again').addEventListener('click', function() {
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15rem';
 
+});
 
-})
+//Lecture Implementing high score - see under when player wins
+
+//Lecture Refactoring code
+
+
